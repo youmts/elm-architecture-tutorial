11bc54467befa8b0592c09af007794a34130d59b
You Matsumoto <youmts22@gmail.com>
2020-03-16 20:48:51 +0900

    feat: svg

--- src/MyTime.elm
+++ src/MyTime.elm
@@ -6,6 +6,8 @@ import Task
 import Time 
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (style)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 main =
   Browser.element
@@ -64,17 +66,54 @@ view model =
     minute = String.fromInt (Time.toMinute model.zone model.time)
     second = String.fromInt (Time.toSecond model.zone model.time)
   in
-  div[]
+  Html.div[]
   [
-    h1
-      [ style "display" "inline-block"
-      , style "border" "solid 2px #aaaaaa"
-      , style "border-radius" "10px"
+    Html.h1
+      [ Html.Attributes.style "display" "inline-block"
+      , Html.Attributes.style "border" "solid 2px #aaaaaa"
+      , Html.Attributes.style "border-radius" "10px"
       ]
-      [ text (hour ++ ":" ++ minute ++ ":" ++ second)
+      [ Html.text (hour ++ ":" ++ minute ++ ":" ++ second)
       ]
-    , button [ onClick TogglePause ] [ text "toggle pause" ]
+    , br [] []
+    , svg
+      [
+        width "240"
+      , height "240"
+      , viewBox "0 0 240 240"
+      ]
+      [ circle
+        [ cx "120"
+        , cy "120"
+        , r "100"
+        , fill "white"
+        , stroke "black"
+        , strokeWidth "2"
+        ]
+        [ 
+        ]
+      , clockHand (toFloat (Time.toHour model.zone model.time) / 12) "Black" "5"
+      , clockHand (toFloat (Time.toMinute model.zone model.time) / 60) "Black" "2"
+      , clockHand (toFloat (Time.toSecond model.zone model.time) / 60) "Red" "2"
+      ]
+    , button [ onClick TogglePause ] [ Html.text "toggle pause" ]
   ]
+
+clockHand: Float -> String -> String -> Svg Msg
+clockHand rate strokeValue strokeWidthValue =
+  line
+    [ x1 "120"
+    , y1 "120"
+    , x2 (String.fromFloat (cos (rateToPi rate) * 100 + 120))
+    , y2 (String.fromFloat (sin (rateToPi rate) * 100 + 120))
+    , stroke strokeValue
+    , strokeWidth strokeWidthValue
+    ]
+    []
+
+rateToPi: Float -> Float
+rateToPi rate =
+  pi * 2 * (rate - 0.25) 
 
 send : msg -> Cmd msg
 send msg =
