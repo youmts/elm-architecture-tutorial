f5e75c98e91fcc6a92c5c7cca728013119459036
You Matsumoto <youmts22@gmail.com>
2020-03-25 10:46:23 +0900

    feat: ボールが壁で反射する

--- src/Arkanoid.elm
+++ src/Arkanoid.elm
@@ -8,6 +8,7 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Task
 import String exposing (fromFloat)
+import Browser.Events exposing (onAnimationFrameDelta)
 
 main = 
   Browser.element
@@ -17,16 +18,26 @@ main =
     , view = view
     }
 
+type alias Vector = (Float, Float)
+
+type alias Ball = 
+  { position : Vector
+  , velocity : Vector
+  }
 type alias Model =
-  {}
+  { ball : Ball
+  }
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Model
+  ( Model 
+    ( Ball (0, 100) (5, 5)
+    )
   , send Nothing
   )
 
 type Msg
     = Nothing
+    | Tick Float
 
 send: Msg -> Cmd Msg
 send msg =
@@ -36,18 +47,57 @@ send msg =
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
   case msg of
-     Nothing ->
+    Nothing ->
       (model, Cmd.none)
+    Tick _ ->
+      ( { model | ball = model.ball |> moveBall |> collisionBall }
+      , Cmd.none
+      )
+
+moveBall : Ball -> Ball
+moveBall old =
+  { old | position = addVector old.position old.velocity}
+
+collisionBall : Ball -> Ball
+collisionBall old =
+  { old |
+    velocity = 
+      if collisionX old.position then reverseX old.velocity
+      else if collisionY old.position then reverseY old.velocity
+      else old.velocity
+  }
+
+collisionX : Vector -> Bool
+collisionX pos =
+  let
+    x = Tuple.first pos
+  in
+    x < 0 || x > w
+
+collisionY : Vector -> Bool
+collisionY pos =
+  let
+    y = Tuple.second pos
+  in
+    y < 0 || y > h
+
+reverseX : Vector -> Vector
+reverseX old = 
+  (-(Tuple.first old), Tuple.second old)
+
+reverseY : Vector -> Vector
+reverseY old = 
+  (Tuple.first old, -(Tuple.second old))
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+  onAnimationFrameDelta Tick
 
 view : Model -> Html Msg
 view model =
   div
     []
-    [ gameField
+    [ gameField model
     ]
 
 w : Float
@@ -56,8 +106,9 @@ w = 600
 h : Float
 h = 800
 
-gameField : Html Msg
-gameField =
+gameField : Model -> Html Msg
+
+gameField model =
   svg
     [ width (fromFloat w)
     , height (fromFloat h)
@@ -70,5 +121,21 @@ gameField =
         , height (fromFloat h)
         , fill "black"
         ]
-        []
+        [ 
+        ]
+    , svgBall model.ball.position
     ]
+
+svgBall : Vector -> Html Msg
+svgBall (x, y) =
+  circle
+    [ cx (fromFloat x)
+    , cy (fromFloat y)
+    , r "10"
+    , fill "red"
+    ]
+    []
+
+addVector : Vector -> Vector -> Vector
+addVector a b =
+   (Tuple.first a + Tuple.first b, Tuple.second a + Tuple.second b)
